421022a624bd1878585197cd45933d077bb5af3e
ishwar.rimal@gmail.com <ishwar.rimal@gmail.com>
2022-01-13 13:13:47 +0530

    some cleanup

--- main.js
+++ main.js
@@ -1,21 +1,13 @@
 // require('dotenv').config()
 import './style.scss';
+import jpt from './jpt';
 
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
 
-const firebaseConfig = {
-  apiKey: import.meta.env.VITE_apiKey,
-  authDomain: import.meta.env.VITE_authDomain,
-  projectId: import.meta.env.VITE_projectId,
-  storageBucket: import.meta.env.VITE_storageBucket,
-  messagingSenderId: import.meta.env.VITE_messagingSenderId,
-  appId: import.meta.env.VITE_appId,
-  measurementId: import.meta.env.VITE_measurementId
-};
 
 if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
+  firebase.initializeApp(jpt);
 }
 const firestore = firebase.firestore();
 
